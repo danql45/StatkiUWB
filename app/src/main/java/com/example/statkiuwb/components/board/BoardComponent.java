a7e1f9af22e264e29ac3ab63b13feac2634e0294
tonio965 <tonio96@o2.pl>
2021-05-23 17:42:01 +0200

    ontouch listener dodany

--- app/src/main/java/com/example/statkiuwb/components/board/BoardComponent.java
+++ app/src/main/java/com/example/statkiuwb/components/board/BoardComponent.java
@@ -1,10 +1,13 @@
-package com.example.statkiuwb.components;
+package com.example.statkiuwb.components.board;
 
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,13 +20,14 @@ import com.example.statkiuwb.systemData.DeviceInfo;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BoardComponent extends ConstraintLayout {
+public class BoardComponent extends ConstraintLayout implements View.OnTouchListener {
 
-    public static String TAG = "com.example.statkiuwb.components.BoardComponent";
+    public static String TAG = "com.example.statkiuwb.components.board.BoardComponent";
     int boardWidth;
-    ConstraintLayout board;
+    FrameLayout board;
     private View root;
     private Context context;
+    int amountOfSquaresInRow = 10;
 
     public BoardComponent(@NonNull Context context) {
         super(context);
@@ -39,6 +43,7 @@ public class BoardComponent extends ConstraintLayout {
                 DeviceInfo.getScreenWidth(context));
         boardWidth = DeviceInfo.getScreenWidth(context);
         board.setLayoutParams(params);
+        board.setOnTouchListener(this);
 
         board.post(() -> setBars());
 
@@ -47,12 +52,12 @@ public class BoardComponent extends ConstraintLayout {
     private void setBars() {
         List<View> bars = new ArrayList<>();
 
-        for(int j=0; j<10; j++){
+        for(int j=0; j<amountOfSquaresInRow; j++){
 
-            int margin = boardWidth/10;
+            int margin = boardWidth/amountOfSquaresInRow;
             //horizontal line
             View hv = new View(context);
-            ConstraintLayout.LayoutParams hParams = new ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT, 1);
+            ConstraintLayout.LayoutParams hParams = new ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT, DeviceInfo.convertDpToPixel(3,context));
             hParams.setMargins(0, (j+1)*margin,0,0);
             hv.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.blueish));
             hv.setLayoutParams(hParams);
@@ -63,7 +68,7 @@ public class BoardComponent extends ConstraintLayout {
 
             //vertival linez
             View vv = new View(context);
-            ConstraintLayout.LayoutParams vParams = new ConstraintLayout.LayoutParams(LayoutParams.MATCH_PARENT, 1);
+            ConstraintLayout.LayoutParams vParams = new ConstraintLayout.LayoutParams(DeviceInfo.convertDpToPixel(3,context), LayoutParams.MATCH_PARENT);
             vParams.setMargins((j+1)*margin, 0,0,0);
             vv.setBackgroundColor(ContextCompat.getColor(getContext(),R.color.blueish));
             vv.setLayoutParams(vParams);
@@ -91,5 +96,53 @@ public class BoardComponent extends ConstraintLayout {
     public BoardComponent(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
         initView(context);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        //czy jest to event puszczonego palca czyli ktos cos wcisnal
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+
+            switch(v.getId()){
+
+                case R.id.boardLayout:
+                    performCLickOnGrid(event);
+                    break;
+            }
+        }
+
+        //zwraca flage p/f czy ten event zostal wykonany - czyli jak false to poleci ten event dalej wglab drzewa layoutu
+        return false;
+    }
+
+    private void performCLickOnGrid(MotionEvent event) {
+
+        int x,y;
+        x= (int)event.getX();
+        y= (int)event.getY();
+
+        //x i y to koordynaty kliku w obrebie planszy czyli niewazne gdzie plansa bedzie na ekranie to lewy gorny rog
+        //planszy zwroci 0,0, a prawy dolny boardWidth,boardWidth
+        //no czyli jak wiemy ile jest whardatow w pionie i poziomie to moge wyliczyc ktory zostal klikniety
+
+
+        for(int i=0; i<amountOfSquaresInRow; i++){
+            for(int j=0; j<amountOfSquaresInRow; j++){
+
+                if(isBetween(x,j) && isBetween(y,i)){
+                    Toast.makeText(context, "coords:"+i+","+j, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    private boolean isBetween(int x, int j) {
+        int squareSize = boardWidth/amountOfSquaresInRow;
+        if(x>j*squareSize && x<(j+1)*squareSize){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
---
+++ app/src/main/java/com/example/statkiuwb/components/board/BoardViewModel.java
@@ -0,0 +1,8 @@
+package com.example.statkiuwb.components.board;
+
+import androidx.lifecycle.ViewModel;
+
+public class BoardViewModel extends ViewModel {
+
+    public static String TAG = "com.example.statkiuwb.components.board.BoardViewModel";
+}
