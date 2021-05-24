7b582b80e0cf18c6a5f0b12b832424676c355173
tonio965 <tonio96>
2021-05-24 23:20:19 +0200

    basic ontouch listeners added to ships but ships cant be placed on board yet

--- app/src/main/java/com/example/statkiuwb/activities/SetBoardActivity.java
+++ app/src/main/java/com/example/statkiuwb/activities/SetBoardActivity.java
@@ -2,7 +2,10 @@ package com.example.statkiuwb.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.example.statkiuwb.R;
 import com.example.statkiuwb.components.board.BoardComponent;
@@ -47,21 +50,39 @@ public class SetBoardActivity extends AppCompatActivity {
     private void initializeTripleShips() {
         verticalTripleShip = findViewById(R.id.tripleShipVertical);
         verticalTripleShip.initView(this,3,true,10);
-
+        verticalTripleShip.setOnTouchListener(new MyTouchListener());
         horizontalTripleShip = findViewById(R.id.tripleShipHorizontal);
         horizontalTripleShip.initView(this,3,false,10);
+        horizontalTripleShip.setOnTouchListener(new MyTouchListener());
     }
 
     private void initializeDoubleShips() {
         verticalDoubleShip = findViewById(R.id.doubleShipVertical);
         verticalDoubleShip.initView(this,2,true,10);
-
+        verticalDoubleShip.setOnTouchListener(new MyTouchListener());
         horizontalDoubleShip = findViewById(R.id.doubleShipHorizontal);
         horizontalDoubleShip.initView(this,2,false,10);
+        horizontalDoubleShip.setOnTouchListener(new MyTouchListener());
     }
 
     private void initializeSingularShip() {
         singularShip = findViewById(R.id.singularShip);
         singularShip.initView(this,1,true,10);
+        singularShip.setOnTouchListener(new MyTouchListener());
+    }
+
+    private final class MyTouchListener implements View.OnTouchListener {
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
+                        view);
+                view.startDrag(data, shadowBuilder, view, 0);
+//                view.setVisibility(View.INVISIBLE);
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
